1588deec36e57d19c1831cdbf07a24c7349fba7f
Diogo Oliveira <jonhydogas@gmail.com>
2019-10-01 23:42:22 +0100

    Add Aula1

---
+++ Práticas/p3/src/aula1/Pessoa.java
@@ -0,0 +1,33 @@
+package aula1;
+
+public class Pessoa {
+	
+	private String nome;
+	private int cc;
+	private Data dataNasc;
+	
+	public Pessoa(String nome, int cc, Data dataNasc) {
+		assert nomeValido(nome) : "Nome inválido";
+		assert ccValido(cc) : "cc inválido";
+		
+		this.nome = nome;
+		this.cc = cc;
+		this.dataNasc = dataNasc;
+	}
+	
+	public String getNome() { return this.nome; }
+	public int getCC() { return this.cc; }
+	public Data getDataNasc() { return this.dataNasc; }
+	
+	public static boolean nomeValido(String nome) {
+		return nome.length() > 0;
+	}
+	
+	public static boolean ccValido(int cc) {
+		return cc > 0;
+	}
+	
+	public String toString() {
+		 return this.nome + " | " + this.cc + " | " + this.dataNasc.toString();
+	}
+}
