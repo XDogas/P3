b332e4afb7a8295fc42a0f48a06bc4d62053d461
Diogo Oliveira <jonhydogas@gmail.com>
2019-10-11 18:22:32 +0100

    Aula1

---
+++ p3/src/aula01/Ex3/Ponto.java
@@ -0,0 +1,24 @@
+package aula01.Ex3;
+
+public class Ponto {
+	
+	private double x;
+	private double y;
+	
+	public Ponto(double x, double y) {		
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX() { return this.x; }
+	public double getY() { return this.y; }
+	
+	public String toString() {
+		return "(" + this.x + ", " + this.y + ")";
+	}
+	
+	public double distTo(Ponto p2) {
+		return Math.sqrt(Math.pow(this.x - p2.getX(), 2) + Math.pow(this.y - p2.getY(), 2));
+	}
+	
+}
